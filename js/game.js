f4064387e843616a69698ae53f70ad60d28458b3
Luscas123 <102263815+Luscas123@users.noreply.github.com>
2023-05-05 21:06:55 -0300

    Update game.js

--- js/game.js
+++ js/game.js
@@ -61,8 +61,7 @@ class Game{
                          fill("red");
                          textSize(20);
                          //adicione o código para exibir o nome do jogador na respectiva cesta
-
-                         
+                         text(allPlayers[index-1].name, x, y-50)                        
                      }
                     
                          textSize(25);
